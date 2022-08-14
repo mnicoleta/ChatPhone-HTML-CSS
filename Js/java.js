ea283a2880f4d38aa1605e6b72214d9bb420ee8b
mnicoleta <nicoleta_miron96@yahoo.com>
2022-08-14 17:34:58 +0300

    first commit

---
+++ Js/java.js
@@ -0,0 +1,25 @@
+ $(document).ready(function(){
+    $('#action_menu_btn').click(function(){
+        $('.action_menu').toggle();
+     });
+         });
+
+
+
+
+  $(document).ready(function() {
+    $("#javaB").addClass("animated shake");
+    $(".contacts").addClass("animated fadeOut")
+   
+
+  });
+
+// function myFunction(){
+//     var x = document.getElementById("action_menu_btn'");
+//     if(x.style.display == "none"){
+//         x.style.display = "block";
+//     }else {
+//         x.style.display ="none";
+//     }
+//  $(".well").addClass("animated shake");
+//$("#target3").addClass("animated fadeOut");}
